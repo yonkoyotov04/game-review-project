06eebd306313cfda8a55a47ecae9b16ee7251eae
yonkoyotov04 <yonkoyotov04@gmail.com>
2025-11-13 13:53:12 +0200

    Initialize project and install dependencies

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # game-review-project
 A project for my ReactJS Course.
+
+- [x] Initialize Project
+- [x] Install needed libraries
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import express from "express"
+import handlebars from "express-handlebars"
+import mongoose from "mongoose"
+import cookieParser from 'cookie-parser'
+
