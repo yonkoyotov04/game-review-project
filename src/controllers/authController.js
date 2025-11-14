70cf66c37ec84027eaca389b5c7f38fc3706be7a
yonkoyotov04 <yonkoyotov04@gmail.com>
2025-11-14 15:50:04 +0200

    Add Register Functionality

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import authService from "../services/authService.js";
+import { getErrorMessage } from "../utils/errorUtils.js";
 
 const authController = Router();
 
@@ -8,6 +10,19 @@ authController.get('/login', (req, res) => {
 
 authController.get('/register', (req, res) => {
     res.render('auth/register');
+})
+
+authController.post('/register', async(req, res) => {
+    const userData = req.body;
+
+    try {
+        const token = await authService.register(userData);
+        res.cookie('auth', token);
+        res.redirect('/');
+    } catch (error) {
+        const errorMessage = getErrorMessage(error);
+        res.status(401).render('auth/register', {error: errorMessage, email: userData.email});
+    }
 })
 
 export default authController;
