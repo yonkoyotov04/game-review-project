5d99c4ce6bf9be1883117d804be95a22522722ab
yonkoyotov04 <yonkoyotov04@gmail.com>
2025-11-14 15:22:53 +0200

    Added token utils

---
+++ src/config/constants.js
@@ -0,0 +1,4 @@
+const JWT_SECRET = 'sdl24ljnfds921904jnxcz8976qwjn23sad'
+
+export default JWT_SECRET
+
---
+++ src/utils/tokenUtils.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import JWT_SECRET from '../config/constants.js'
+
+export function generateAuthToken(user) {
+    const payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }
+
+    const token = jwt.sign(payload, JWT_SECRET, {expiresIn: '2h'})
+
+    return token;
+}
