581e26a1b2a8a3ebf590934ed879c2e0eb972c81
yonkoyotov04 <yonkoyotov04@gmail.com>
2025-11-22 14:19:04 +0200

    Updated auth service

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -18,7 +18,12 @@ export default {
         const user = await User.create(userData);
 
         const token = generateAuthToken(user);
-        return token;
+
+        return {
+            _id: user.id,
+            email: user.email,
+            accessToken: token
+        };
     },
 
     async login(email, password) {
@@ -35,7 +40,12 @@ export default {
         }
 
         const token = generateAuthToken(user);
-        return token;
+
+        return {
+            _id: user.id,
+            email: user.email,
+            accessToken: token
+        };
     },
 
     getProfileData(id) {
